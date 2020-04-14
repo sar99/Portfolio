79b95a4f83de761ddf49b85b4a003745ba48474a
sar99 <43995983+sar99@users.noreply.github.com>
2020-04-15 02:11:11 +0530

    add inline styling to nav

--- src/js/nav.js
+++ src/js/nav.js
@@ -6,9 +6,9 @@ import { list } from './data.js'
 function _navbar() {
     return (
         < div className="navi" id="navi" >
-            <div className="navi-bg"></div>
-            <div className="navi-bg blur"></div>
-            <div className="content flexrow">
+            <div className="navi-bg" style={{opacity: "83%"}}></div>
+            <div className="navi-bg blur" style={{opacity: "83%"}}></div>
+            <div className="content flexrow" style={{opacity: "83%"}}>
 
                 {list.map((x, i) => {
                     return (<div key={i} className="navitem"><a href={"#NAV" + x}>{x}</a></div>)
